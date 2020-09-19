da3f272cc1d9e64d4cc704d738e6ff7f39e463d3
Henrique Tavares <ihenrits@gmail.com>
2020-09-19 16:57:11 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85c753fad00ba164ef0fa275dca76d52'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3c0a03ad-b12d-442e-a6f1-76814ca91769'
+};
